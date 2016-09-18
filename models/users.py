05686cf2a0782294ab943bd86d5ae0a5480b8dce
Stephen Michelini <sdmichelini@wpi.edu>
2016-09-18 12:52:00 -0400

    Added People to the Points System

--- models/users.py
+++ models/users.py
@@ -1,6 +1,32 @@
 USERS = {
-	'test@example.com':'Test User',
-	'test@gmail.com':'Gmail User'
+	'sdmichelini@gmail.com':'Stephen Michelini',
+	'jswitz16@gmail.com':'Joe Switzer',
+	'bpierce900@gmail.com':'Bo Pierce',
+	'Matthewfreed217@gmail.com':'Matt Freed',
+	'Wkwiecinski97@gmail.com':'Wally',
+	'jchakalos1@gmail.com':'James Chakalos',
+	'eriksola1996@gmail.com':'Erik Sola',
+	'Luke328995@gmail.com':'Lucas Chico',
+	'mfmurphyaero@gmail.com':'Matt Murphy',
+	'22.mbeaulieu@gmail.com':'Daddy',
+	'Petedipers@gmail.com':'Peter',
+	'rgm22011@gmail.com':'Ryan McLaughlin',
+	'bpwagner15@gmail.com':'Ben Wagner',
+	'kevinguth1@gmail.com':'Kevin Guth',
+	'stephenpeccerillo@gmail.com':'Stephen Peccerillo',
+	'Rhibbard16@gmail.com':'Ron hibbard',
+	'sam.c.coache@gmail.com':'Sam Coache',
+	'mpmmills@outlook.com':'Matthew J. Milliken',
+	'Pshingletoniv@gmail.com':'Paul Shingleton',
+	'mattmcd25@gmail.com':'bean bag',
+	'clarkbrandon364@gmail.com':'Brandon Clark',
+	'bakerjackson14@gmail.com':'Jackson Baker',
+	'dakruzan@gmail.com':'Derek Kruzan',
+	'dalilplatypus@gmail.com':'Scott Friedlander',
+	'brian.praetorius@gmail.com':'Brian Praetorius',
+	'zligham@gmail.com':'Ligham',
+	'willbennett1301@gmail.com':'Bennett',
+	'jvb2358@gmail.com':'Jon Van Blarcum'
 }
 
 def get_all_users():
